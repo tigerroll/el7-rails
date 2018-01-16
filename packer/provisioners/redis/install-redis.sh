1bceed968706f85f5e93c38ea4d8a230eaef0fa2
tigerroll <tigerroll54s@gmail.com>
2018-01-16 10:22:25 +0900

    redis firewall modify settings.

--- packer/provisioners/redis/install-redis.sh
+++ packer/provisioners/redis/install-redis.sh
@@ -4,9 +4,13 @@
 yum -y --enablerepo=remi,remi-test,epel install redis
 redis --version
 
-# Firewall settings.(mysqld)
-iptables -I INPUT -p tcp -m tcp --dport 6379 -j ACCEPT
-service iptables save
+# Firewall settings.(nginx)
+type firewall-cmd > /dev/null 2>&1 && (
+  firewall-cmd --reload;
+  firewall-cmd --zone=public --add-port=6379/tcp --permanent;
+  firewall-cmd --reload;
+  firewall-cmd --list-all;
+)
 
 # auto start configure.
 systemctl enable redis.service
